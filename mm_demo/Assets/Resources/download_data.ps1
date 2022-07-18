4e4d6e7426ec14749610a2c3382772aaf38ae5e7
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-07-19 00:11:20 +0200

    Removed other motion matching files and added links instead

--- mm_demo/Assets/Resources/download_data.ps1
+++ mm_demo/Assets/Resources/download_data.ps1
@@ -1,2 +1,4 @@
 Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/t7oZSyHQk8g3aWy/download/database_merengue_raw.bin.txt  -OutFile "database_merengue_raw.bin.txt"
 Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/WTgqLpaxsWWG9io/download/database_merengue.bin.txt -OutFile "database_merengue.bin.txt"
+Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/otqsPzQSgTr548S/download/loco_database.bin.txt -OutFile "loco_database.bin.txt"
+Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/otqsPzQSgTr548S/download/loco_database.bin.txt -OutFile "database_dance.bin.txt"
