9c5eb677d4bcd37a97f1b1aa552e2a49cbf2c761
Erik Herrmann <erik.herrmann@dfki.de>
2022-10-12 15:30:33 +0200

    Moved frame_idx to MMPose class

--- mm_preprocessing/motion_matching/mm_controller.py
+++ mm_preprocessing/motion_matching/mm_controller.py
@@ -5,24 +5,23 @@ from .mm_pose import MMPose
 class MMController():
     def __init__(self) -> None:
         self.target_dir = np.array([0, 0, 1])
+        self.offset = [0,0.1,0]
         self.max_step_length = 10
         self.search_interval = 1#0.05
         self.force_search_timer =0
         self.mm_database = None
         self.pose = None
         self.n_frames = 0
-        self.frame_idx = 0
         self.frame_time = 1/30
 
         self.target_skeleton = None
         self.retargeting = None
         self.target_controller = None
         self.update_timer = 0
-        self.use_velocity = True
 
     def set_data(self, mm_database, scale=1.0):
         self.mm_database = mm_database
-        self.pose = MMPose.from_db(mm_database, 0, scale)
+        self.pose = MMPose.from_db(mm_database, 0, scale, self.offset)
         self.frame_time = 1.0/self.mm_database.fps
         self.n_frames = len(mm_database.bone_positions)
 
@@ -35,45 +34,44 @@ class MMController():
         self.target_skeleton = target_controller.get_skeleton()
         joint_map = generate_joint_map(src_skeleton.skeleton_model, self.target_skeleton.skeleton_model)
         self.retargeting = Retargeting(src_skeleton, self.target_skeleton, joint_map)
-
-
+   
     def update(self, dt):
         if self.force_search_timer > 0:
             self.force_search_timer -= dt
         #print(self.force_search_timer)
-        if self.frame_idx >= self.n_frames or self.force_search_timer <= 0:
-            self.find_transition()
+        if self.pose.frame_idx >= self.n_frames or self.force_search_timer <= 0:
+            self.find_transition(self.pose)
             self.force_search_timer = self.search_interval
         if self.update_timer >= 0:
             self.update_timer -= dt
         if self.update_timer <=0:
-            self.frame_idx +=1
-        if self.frame_idx >= self.n_frames:
-            self.frame_idx =0
-        self.pose.set_pose(self.mm_database, self.frame_idx)
+            self.pose.frame_idx +=1
+        if self.pose.frame_idx >= self.n_frames:
+            self.pose.frame_idx =0
+        self.pose.set_pose(self.mm_database, self.pose.frame_idx)
         self.pose.update_sim(dt)
-        return self.frame_idx
+        return self.pose.frame_idx
 
-
-    def find_transition(self):
-        next_frame_idx = min(self.frame_idx + 1, self.n_frames-1)
-        self.frame_idx = self.mm_database.find_transition(self.pose, next_frame_idx)
-        if self.frame_idx > self.n_frames:
-            self.frame_idx = 0
-
-    def update_w(self, dt):
-        if self.force_search_timer > 0:
-            self.force_search_timer -= dt
-        self.pose.update_sim(dt)
-        if self.frame_idx >= self.n_frames or self.force_search_timer <= 0:
-            self.find_transition()
-            self.force_search_timer = self.search_interval
-        self.animation_time += dt
-        self.frame_idx = int(self.animation_time / self.frame_time)
-        self.pose.set_pose(self.mm_database, self.frame_idx)
-        return self.frame_idx, self.animation_time
+    def find_transition(self, pose):
+        next_frame_idx = min(pose.frame_idx + 1, self.n_frames-1)
+        pose.frame_idx = self.mm_database.find_transition(pose, next_frame_idx)
+        if pose.frame_idx > self.n_frames:
+            pose.frame_idx = 0
 
     def get_pose(self):
+        self.pose.update_fk_buffer()
+        n_dims = (self.pose.n_bones-1)*4 +3
+        frame = np.zeros(n_dims)
+        frame[:3] = self.pose.global_positions[1]
+        frame[3:7] = self.pose.global_rotations[1]
+        o = 7
+        for i in range(2, self.pose.n_bones):
+            frame[o:o+4] = self.pose.rotations[i]
+            o += 4
+        return frame
+
+    def get_pose_sim_pos(self):
+        """ return sim position """
         self.pose.update_fk_buffer()
         n_dims = (self.pose.n_bones-1)*4 +3
         frame = np.zeros(n_dims)
@@ -93,17 +91,17 @@ class MMController():
 
     def get_retargeted_pose(self):
         ref_frame = self.target_skeleton.reference_frame
-        src_frame = self.get_pose()
+        src_frame = self.get_pose_sim_pos()
         target_frame = self.retargeting.retarget_frame(src_frame, ref_frame)
         return target_frame
 
-    def reset(self):
-        self.frame_idx = self.start_frame_idx
+    def reset(self, frame_idx):
         self.update_timer = 0.0
+        self.pose.reset(frame_idx)
 
     def get_phase(self):
-        return self.mm_database.phase_data[self.frame_idx]
-
+        return self.mm_database.phase_data[self.pose.frame_idx]
+        
     def rotate_dir_vector(self, angle):
         r = np.radians(angle)
         s = np.sin(r)
@@ -113,3 +111,11 @@ class MMController():
         self.target_dir[2] = s * self.target_dir[0] + c * self.target_dir[2]
         self.target_dir /= float(np.linalg.norm(self.target_dir))
         print("rotate", self.target_dir)
+        
+
+    def get_relative_bone_velocity(self, bone_idx):
+        return self.mm_database.get_relative_bone_velocity(self.pose.frame_idx, bone_idx)
+
+    def get_relative_bone_velocities_frame(self):
+        return self.mm_database.get_relative_bone_velocities_frame(self.pose.frame_idx)
+
--- mm_preprocessing/motion_matching/mm_pose.py
+++ mm_preprocessing/motion_matching/mm_pose.py
@@ -1,17 +1,21 @@
+
 import numpy as np
 from . import quat
 from transformations import quaternion_matrix, quaternion_from_matrix, quaternion_from_euler
 
+
 class MMPose:
-    def __init__(self, parents, scale=1, use_velocity=True)-> None:
-        
+    def __init__(self, parents, scale=1, use_velocity=True, offset=None)-> None:
+        if offset is None:
+            offset = np.zeros(3)
+        self.offset = offset
         self.parents = parents
         self.n_bones = len(self.parents)
         self.positions = np.zeros((self.n_bones, 3))
         self.rotations = np.zeros((self.n_bones, 4))
         self.global_positions = np.zeros((self.n_bones, 3))
         self.global_rotations = np.zeros((self.n_bones, 4))
-        self.sim_position = np.array([0,0,0], dtype=np.float32)
+        self.sim_position = np.array(self.offset, dtype=np.float32)
         self.sim_rotation = np.array([1,0,0,0], dtype=np.float32)
         self.lin_vel = np.array([0,0,0], dtype=np.float32)
         self.ang_vel = np.array([0,0,0], dtype=np.float32)
@@ -19,32 +23,39 @@ class MMPose:
         self.scale = scale
         self.ref_position = np.array([0,0,0], dtype=np.float32)
         self.ref_rotation = np.array([1,0,0,0], dtype=np.float32)
+        self.frame_idx = 0
 
     @classmethod
-    def from_db(cls, db, frame_idx, scale=1.0):
-        pose = MMPose(db.bone_parents, scale)
+    def from_db(cls, db, frame_idx, scale=1, offset=None):
+        pose = MMPose(db.bone_parents, scale, offset=offset)
         pose.set_pose(db, frame_idx)
         return pose
 
     def set_pose(self, db, frame_idx):
+        self.frame_idx = frame_idx
         self.global_positions = np.zeros((self.n_bones, 3))
         self.global_rotations = np.zeros((self.n_bones, 4))
-        self.positions = db.bone_positions[frame_idx][:]*self.scale
-        self.rotations = db.bone_rotations[frame_idx][:]
+        self.positions = np.array(db.bone_positions[frame_idx])*self.scale
+        self.rotations = np.array(db.bone_rotations[frame_idx])
         if self.use_velocity:
             self.positions[0] = self.sim_position
             self.rotations[0] = self.sim_rotation
-        self.ref_position = db.bone_positions[frame_idx,0]*self.scale
-        self.ref_rotation = db.bone_rotations[frame_idx,0]
+        self.ref_position = np.array(db.bone_positions[frame_idx,0])*self.scale
+        self.ref_rotation = np.array(db.bone_rotations[frame_idx,0])
         inv_root_rot = quat.normalize(quat.inv(self.ref_rotation))
         self.lin_vel = quat.mul_vec(inv_root_rot, db.bone_velocities[frame_idx,0])*self.scale
         self.ang_vel = -quat.mul_vec(inv_root_rot, db.bone_angular_velocities[frame_idx,0])
         self.update_fk_buffer()
 
     def update_sim(self, dt):
-        velocity = quat.mul_vec(self.sim_rotation, self.lin_vel*dt)
-        av = quat.mul_vec(self.sim_rotation, self.ang_vel*dt)
-        delta_rot = quat.from_euler(np.array([0, 0,av[1]]), "zxy")
+        #delta_pos  = quat.mul_vec(self.sim_rotation, self.lin_vel*dt)
+        #self.sim_position += delta_pos
+        #global_ang_vel = quat.mul_vec(self.sim_rotation, self.ang_vel*dt)
+        #delta_rot = quat.from_euler(global_ang_vel)
+        #self.sim_rotation = quat.mul(self.sim_rotation, delta_rot)
+        velocity = quat.mul_vec(self.sim_rotation, self.lin_vel)*dt
+        av = quat.mul_vec(self.sim_rotation, self.ang_vel)*dt
+        delta_rot = quat.from_euler(av, "xyz")
         self.sim_position += velocity
         self.sim_rotation = quat.normalize(quat.mul(delta_rot, self.sim_rotation))
 
@@ -66,6 +77,13 @@ class MMPose:
         else:
             self.global_positions[bone_idx] = self.positions[bone_idx]
             self.global_rotations[bone_idx] = self.rotations[bone_idx]
+
+    def reset(self, frame_idx):
+        self.frame_idx = frame_idx#self.start_frame_idx
+        self.sim_position = np.array(self.offset, dtype=np.float32)
+        self.sim_rotation = np.array([1,0,0,0], dtype=np.float32)
+        self.lin_vel = np.array([0,0,0], dtype=np.float32)
+        self.ang_vel = np.array([0,0,0], dtype=np.float32)
             
     def get_linear_velocity(self):
         return quat.mul_vec(self.sim_rotation, self.lin_vel)
