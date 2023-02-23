181ed9b12a089b7b17121c1d1ab9420e9e2e6550
Erik Herrmann <erik.herrmann@dfki.de>
2023-02-23 16:54:10 +0100

    Added examples using the numpy format for merengue

--- mm_demo/Assets/Resources/download_data.ps1
+++ mm_demo/Assets/Resources/download_data.ps1
@@ -2,3 +2,6 @@ Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/t7oZSyHQk8g3aW
 Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/WTgqLpaxsWWG9io/download/database_merengue.bin.txt -OutFile "database_merengue.bin.txt"
 Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/otqsPzQSgTr548S/download/loco_database.bin.txt -OutFile "loco_database.bin.txt"
 Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/otqsPzQSgTr548S/download/loco_database.bin.txt -OutFile "database_dance.bin.txt"
+
+Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/MoBYPPd6X99sxQX/download/database_merengue_raw7.npz.txt -OutFile "database_merengue_raw7.npz.txt"
+Invoke-WebRequest -Uri https://cloud.dfki.de/owncloud/index.php/s/SSyba6eBgY4Rwgp/download/database_merengue_raw25.npz.txt -OutFile "database_merengue_raw25.npz.txt"
